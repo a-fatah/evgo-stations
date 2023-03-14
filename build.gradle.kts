8ff7d2172872364505c581e2826d8ce126e4a51a
a-fatah <abdulfatah66@gmail.com>
2023-03-14 02:18:01 +0100

    configure database access

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.7.22"
+    kotlin("plugin.allopen") version "1.7.22"
 }
+
 
 group = "io.evgo"
 version = "0.0.1-SNAPSHOT"
@@ -20,10 +23,15 @@ extra["springBootAdminVersion"] = "3.0.0-M4"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.26")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("de.codecentric:spring-boot-admin-starter-server")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // add test dependencies here
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("com.h2database:h2")
+
 }
 
 dependencyManagement {
