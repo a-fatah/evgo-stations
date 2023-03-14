c8070c19aafe8a219cac90a2ce0dc4da59db1c2b
a-fatah <abdulfatah66@gmail.com>
2023-03-14 01:55:12 +0100

    refine data model

--- src/main/kotlin/io/evgo/stations/ChargingStation.kt
+++ src/main/kotlin/io/evgo/stations/ChargingStation.kt
@@ -1,26 +1,38 @@
 package io.evgo.stations
 
+import jakarta.persistence.*
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
 
+@Entity
 data class ChargingStation(
-    val id: Int,
-    val name: String,
-    val address: String,
-    val location: Location,
-    val addressInfo: AddressInfo,
-    val connectors: List<Connector>,
-    val pricing: Pricing,
-    val amenities: List<String>,
-    val openingHours: List<String>,
-    val rating: Double
+    @Id
+    @GeneratedValue
+    val id: Int? = null,
+    val name: String = "",
+    val address: String = "",
+    val location: String = "", // will be a geo-location
+    @OneToOne
+    val addressInfo: AddressInfo? = null,
+    @OneToMany
+    val connectors: List<Connector> = emptyList(),
+    @OneToOne
+    val pricing: Pricing? = null,
+    @ElementCollection
+    val amenities: List<String> = emptyList(),
+    @ElementCollection
+    val openingHours: List<String> = emptyList(),
+    val rating: Double = 0.0,
 )
 
+@Entity
 data class Connector(
-    val id: Int,
-    val type: ConnectorType,
-    val power: Int,
-    val status: ConnectorStatus
+    @Id
+    @GeneratedValue
+    val id: Int? = null,
+    @Enumerated(EnumType.STRING)
+    val type: ConnectorType = ConnectorType.TYPE_2,
+    val power: Int = 0, // in kW
+    val status: ConnectorStatus = ConnectorStatus.AVAILABLE
 )
 
 enum class ConnectorStatus {
@@ -31,22 +43,25 @@ enum class ConnectorType {
     TYPE_1, TYPE_2, CCS, DC_FAST, CHADEMO
 }
 
-data class Location(
-    val latitude: Double,
-    val longitude: Double
-)
-
+@Entity
 data class AddressInfo(
-    val country: String,
-    val city: String,
-    val street: String,
-    val houseNumber: String,
-    val postalCode: String
+    @Id
+    @GeneratedValue
+    val id: Int? = null,
+    val country: String = "DE",
+    val city: String = "Berlin",
+    val street: String = "",
+    val houseNumber: String = "",
+    val postalCode: String = ""
 )
 
+@Entity
 data class Pricing(
-    val currency: String,
-    val pricePerKwh: Double
+    @Id
+    @GeneratedValue
+    val id: Int? = null,
+    val currency: String = "EUR",
+    val pricePerKwh: Int = 0, // in cents
 )
 
 
