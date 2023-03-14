108970e0317bb41d8790723165fd88fe477fcbde
a-fatah <abdulfatah66@gmail.com>
2023-03-14 04:19:05 +0100

    add seed data if flag is set

--- src/main/kotlin/io/evgo/stations/ChargingStation.kt
+++ src/main/kotlin/io/evgo/stations/ChargingStation.kt
@@ -9,29 +9,40 @@ import java.time.LocalDate
 @Entity
 data class ChargingStation(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
     val name: String = "",
-    val address: String = "",
     val location: String = "", // will be a geo-location
-    @ManyToMany(cascade = [CascadeType.ALL])
+    @ManyToMany
     @JoinTable(
         name = "charging_station_connector",
         joinColumns = [JoinColumn(name = "station_id")],
         inverseJoinColumns = [JoinColumn(name = "connector_id")]
     )
     val connectors: List<Connector> = emptyList(),
-    @ElementCollection
-    val amenities: List<String> = emptyList(),
-    @ElementCollection
-    val openingHours: List<String> = emptyList(),
+    @ManyToMany(cascade = [CascadeType.ALL])
+    @JoinTable(
+        name = "charging_station_amenity",
+        joinColumns = [JoinColumn(name = "station_id")],
+        inverseJoinColumns = [JoinColumn(name = "amenity_id")]
+    )
+    val amenities: List<Amenity> = emptyList(),
+    val openingHours: String = "",
     val rating: Double = 0.0,
+)
+
+@Entity
+data class Amenity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int? = null,
+    val name: String = "",
 )
 
 @Entity
 data class Connector(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
     @Enumerated(EnumType.STRING)
     @Column(name = "connectorType")
@@ -49,6 +60,8 @@ enum class ConnectorType {
     TYPE_1, TYPE_2, CCS, DC_FAST, CHADEMO
 }
 
+interface ConnectorRepository: JpaRepository<Connector, Int> {
+}
 
 interface ChargingStationRepository: JpaRepository<ChargingStation, Int> {
 }
--- src/main/kotlin/io/evgo/stations/StationsApplication.kt
+++ src/main/kotlin/io/evgo/stations/StationsApplication.kt
@@ -1,64 +1,110 @@
 package io.evgo.stations
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class StationsApplication
+class StationsApplication {
+
+    @Bean
+    @ConditionalOnProperty(name = ["init-db"], havingValue = "true")
+    fun initDb(
+        chargingStationRepository: ChargingStationRepository,
+        connectorRepository: ConnectorRepository
+    ) = CommandLineRunner {
+
+        val locations = listOf(
+            "Alexander Platz 1, 10178 Berlin",
+            "Kurfürstendamm 1, 10719 Berlin",
+            "Friedrichstraße 1, 10117 Berlin",
+            "Potsdamer Platz 1, 10785 Berlin",
+            "Unter den Linden 1, 10117 Berlin",
+            "Karl-Liebknecht-Straße 1, 10178 Berlin",
+            "Leipziger Platz 1, 10117 Berlin",
+        )
+
+        val connectorTypes = listOf(
+            ConnectorType.TYPE_1,
+            ConnectorType.TYPE_2,
+            ConnectorType.CCS,
+            ConnectorType.DC_FAST,
+        )
+
+        val connectorPowers = listOf(3, 7, 22, 50)
+        val connectorPrices = listOf(10, 20, 30, 40)
+
+        // associate each connector type with a power
+        val connectorTypePower = connectorTypes.zip(connectorPowers).toMap()
+
+        val connectorTypePrice = connectorTypes.zip(connectorPrices).toMap()
+
+        // create list of connectors with random status
+        var connectors = connectorTypes.map { type ->
+            Connector(
+                type = type,
+                power = connectorTypePower[type] ?: 0,
+                price = connectorTypePrice[type] ?: 0,
+                status = if (Math.random() > 0.5) ConnectorStatus.AVAILABLE else ConnectorStatus.OCCUPIED
+            )
+        }
+
+        // save connectors to database
+        connectors = connectorRepository.saveAll(connectors)
+
+        val amenities = listOf("WC", "Coffee", "Parking", "Restaurant", "Snacks")
+
+        // create a function that returns a random list of amenities
+        fun randomAmenities(): List<Amenity> {
+            val randomAmenities = mutableListOf<Amenity>()
+            amenities.forEach { amenity ->
+                if (Math.random() > 0.5) {
+                    randomAmenities.add(Amenity(name = amenity))
+                }
+            }
+            return randomAmenities
+        }
+
+        locations.forEach { location ->
+            val station = ChargingStation(
+                name = "Station $location",
+                location = location,
+                connectors = connectors,
+                amenities = randomAmenities(),
+                openingHours = listOf("Mo-Fr 8-18", "Sa 10-16").joinToString(", "),
+                rating = Math.random() * 5
+            )
+            chargingStationRepository.save(station)
+        }
+    }
+
+}
+
 
 fun main(args: Array<String>) {
-    val stations = listOf<ChargingStation>(
-        ChargingStation(
-            name = "Station 1",
-            address = "Address 1",
-            location = "Location 1",
-            connectors = listOf(
-                Connector(
-                    type = ConnectorType.TYPE_2,
-                    power = 22,
-                    status = ConnectorStatus.AVAILABLE
-                ),
-                Connector(
-                    type = ConnectorType.CCS,
-                    power = 50,
-                    status = ConnectorStatus.AVAILABLE
-                ),
-                Connector(
-                    type = ConnectorType.DC_FAST,
-                    power = 150,
-                    status = ConnectorStatus.AVAILABLE
-                ),
-            ),
-            amenities = listOf("WC", "Shower"),
-            openingHours = listOf("Mo-Fr 8-18", "Sa 10-16"),
-            rating = 4.5
-        ),
-        ChargingStation(
-            name = "Station 2",
-            address = "Address 2",
-            location = "Location 2",
-            connectors = listOf(
-                Connector(
-                    type = ConnectorType.TYPE_2,
-                    power = 22,
-                    status = ConnectorStatus.AVAILABLE
-                ),
-                Connector(
-                    type = ConnectorType.CCS,
-                    power = 50,
-                    status = ConnectorStatus.AVAILABLE
-                ),
-                Connector(
-                    type = ConnectorType.DC_FAST,
-                    power = 150,
-                    status = ConnectorStatus.AVAILABLE
-                ),
-            ),
-            amenities = listOf("WC", "Shower"),
-            openingHours = listOf("Mo-Fr 8-18", "Sa 10-16"),
-            rating = 4.5
-        ),
+
+    val locations = listOf(
+        "Alexander Platz 1, 10178 Berlin",
+        "Kurfürstendamm 1, 10719 Berlin",
+        "Friedrichstraße 1, 10117 Berlin",
+        "Potsdamer Platz 1, 10785 Berlin",
+        "Unter den Linden 1, 10117 Berlin",
+        "Karl-Liebknecht-Straße 1, 10178 Berlin",
+        "Leipziger Platz 1, 10117 Berlin",
     )
+
+    val connectorTypes = listOf(
+        ConnectorType.TYPE_2,
+        ConnectorType.CCS,
+        ConnectorType.DC_FAST,
+    )
+
+    //
+
+
+
     runApplication<StationsApplication>(*args)
 }
 
