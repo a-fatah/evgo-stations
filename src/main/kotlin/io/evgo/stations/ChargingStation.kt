a010f9c5cf2673cb0f6e051e68c10bddde0cee19
a-fatah <abdulfatah66@gmail.com>
2023-03-31 22:18:00 +0200

    modify type of location to geometric point

--- src/main/kotlin/io/evgo/stations/ChargingStation.kt
+++ src/main/kotlin/io/evgo/stations/ChargingStation.kt
@@ -1,6 +1,8 @@
 package io.evgo.stations
 
 import jakarta.persistence.*
+import org.locationtech.jts.geom.Point
+
 
 @Entity
 data class ChargingStation(
@@ -8,7 +10,8 @@ data class ChargingStation(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
     val name: String = "",
-    val location: String = "", // will be a geo-location
+    @Column(columnDefinition = "geometry(Point,4326)")
+    val location: Point? = null, // will be a geo-location
     @ManyToMany
     @JoinTable(
         name = "charging_station_connector",
