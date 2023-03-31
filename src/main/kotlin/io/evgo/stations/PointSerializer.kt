bc36a4c9843c9e0e3c318f5bdfde4326cc630838
a-fatah <abdulfatah66@gmail.com>
2023-04-01 01:28:17 +0200

    serialize Point to json

---
+++ src/main/kotlin/io/evgo/stations/PointSerializer.kt
@@ -0,0 +1,15 @@
+package io.evgo.stations
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import org.locationtech.jts.geom.Point
+
+class PointSerializer: JsonSerializer<Point>() {
+    override fun serialize(value: Point, gen: JsonGenerator, serializers: SerializerProvider) {
+        gen.writeStartObject()
+        gen.writeNumberField("longitude", value.x)
+        gen.writeNumberField("latitude", value.y)
+        gen.writeEndObject()
+    }
+}
--- src/main/kotlin/io/evgo/stations/Repository.kt
+++ src/main/kotlin/io/evgo/stations/Repository.kt
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import org.springframework.hateoas.EntityModel
-import org.springframework.hateoas.server.RepresentationModelProcessor
 
 @RepositoryRestResource(path="stations", itemResourceRel = "station", collectionResourceRel = "stations")
 interface ChargingStationRepository: JpaRepository<ChargingStation, Int> {
@@ -18,13 +16,4 @@ interface ChargingStationRepository: JpaRepository<ChargingStation, Int> {
 }
 
 @RepositoryRestResource
-interface ConnectorRepository: JpaRepository<Connector, Int> {
-}
-
-class ConnectorResourceProcessor : RepresentationModelProcessor<EntityModel<Connector>> {
-
-    override fun process(model: EntityModel<Connector>): EntityModel<Connector> {
-        TODO("Not yet implemented")
-    }
-
-}
+interface ConnectorRepository: JpaRepository<Connector, Int>
